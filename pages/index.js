5fb550f343ca0790a4c8b394780762c012179d40
Kian Sam <itskiansam@gmail.com>
2021-06-21 10:54:23 +0300

    Results.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 
 export default function Home() {
   return (
@@ -12,8 +13,8 @@ export default function Home() {
       </Head>
 
       <Header />
-
       <Nav />
+      <Results />
     </div>
   );
 }
